40fdb0026b684af6e43db037f297d977d9a5f21b
sylvia_mavuba <smavuba@voyages-sncf.com>
2016-06-03 15:57:37 +0200

    change port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var logger = require('morgan');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs'); // moteur template
 app.set('views', path.join(__dirname, '/views')); //dossier pages
@@ -11,6 +12,6 @@ app.get('/', function (req, res) {
   res.render('index');
 }); 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port!' + port);
 });
